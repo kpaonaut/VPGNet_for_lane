9e14565db8be1a4704c33d68fb6d1978ace06af1
Rui Wang <1172094131@qq.com>
2018-08-08 14:30:06 -0700

    reverted back to make it work

--- caffe/models/vpgnet-novp/script-rui/run_model.py
+++ caffe/models/vpgnet-novp/script-rui/run_model.py
@@ -6,7 +6,8 @@ import time # time the execution time
 import caffe
 import cv2
 
-model = '/home/rui/VPGNet/caffe/models/vpgnet-novp/deploy_Rui.prototxt' # deploy_Rui: pruned useless branches
+# model = '/home/rui/VPGNet/caffe/models/vpgnet-novp/deploy_Rui.prototxt' # deploy_Rui: pruned useless branches
+model = '/home/rui/VPGNet/caffe/models/vpgnet-novp/deploy.prototxt' # original deploy, no pruning
 pretrained = '/home/rui/VPGNet/caffe/models/vpgnet-novp/snapshots/split_iter_100000.caffemodel'
 
 caffe.set_mode_gpu()
@@ -25,12 +26,8 @@ t = time.time()
 transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
 transformer.set_transpose('data', (2, 0, 1))  # move image channels to outermost dimension
 transformer.set_raw_scale('data', 255)      # rescale from [0, 1] to [0, 255]
-transformed_img = transformer.preprocess('data', img)
-# print transformed_img
-# swap R, B channel, the final input to the network should be RGB
-tmp = np.copy(transformed_img[0])
-transformed_img[0] = transformed_img[2]
-transformed_img[2] = tmp
+transformer.set_channel_swap('data', (2, 1, 0))
+transformed_img = transformer.preprocess('data', img) # swap R, B channel, the final input to the network should be RGB
 
 net.blobs['data'].data[...] = transformed_img
 t1 = time.time()
